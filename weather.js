1982b3a116a299f276cc6bb28261f04b486f4eeb
dyuthi <dyuthilaya2020@gmail.com>
2026-09-23 12:19:29 +0530

    final

--- weather.js
+++ weather.js
@@ -16,38 +16,25 @@ document.querySelector('#myform').addEventListener('submit',(e)=>{
         let data7=info.data.weather[0].description;
         let data8=info.data.wind.speed;
         let data9=info.data.main.sea_level;
-        
-        document.querySelector('#result1').innerHTML=
-        `
-        <section id="result">
-        <div id="heading">
-                <p class="para" > Location :</p>
-                <p class="para" > Temparature :</p>
-                <p class="para" > Feels_like :</p>
-                <p class="para" > Humidity :</p>
-                <p class="para" > Pressure :</p>
-                <p class="para">Weather :</p>
-                <p class="para">Description :</p>
-                <p class="para">Wind Speed :</p>
-                <p class="para">Sea level :</p>
-                
-            </div>
-            <div id="values">
-                <p class="para" id="data1">${data1}</p>
-                <p class="para" id="data2">${data2}</p>
-                <p class="para" id="data3">${data3}</p>
-                <p class="para" id="data4">${data4}</p>
-                <p class="para" id="data5">${data5}</p>
-                <p class="para" id="data6">${data6}</p>
-                <p class="para" id="data7">${data7}</p>
-                <p class="para" id="data8">${data8}</p>
-                <p class="para" id="data9">${data9}</p>
-            </div>
-            </section>`;
-            if(data6==="Clouds"){
+        let data = [data1, data2, data3, data4, data5, data6, data7, data8, data9];
+        let heading = ["Location", "Temperature", "Feels Like", "Humidity", "Pressure", "Weather", "Description", "Wind Speed", "Sea Level"];
+        let output = "";
+        for (let i = 0; i < 9; i++) {
+            output += `
+            <div class="row">
+                <div class="col-2" id="heading">
+                    <p class="para">${heading[i]}</p>
+                </div>
+                <div class="col-2" id="values">
+                    <p class="para">${data[i]}</p>
+                </div>
+            </div>`;
+            }
+        document.querySelector('#result1').innerHTML = `<section id="container">${output}</section>`;
+        if(data6==="Clouds"){
             document.querySelector('body').style.backgroundImage="url('https://t3.ftcdn.net/jpg/07/94/66/96/360_F_794669630_EqED5xzraTaXKgADnKyti75HZgRPy7Ar.jpg')";
         }else if(data6==="Rain"){
-            document.querySelector('body').style.backgroundImage="url('https://images.rawpixel.com/image_800/cHJpdmF0ZS9sci9pbWFnZXMvd2Vic2l0ZS8yMDI0LTEwL3Jhd3BpeGVsb2ZmaWNlNl9waG90b19vZl9hX2RhcmtfZ3JleV9yYWluX2Nsb3VkX3BuZ19pc29sYXRlZF9vbl8zZGI1YWIyNS1lMGI1LTQzNDYtYTk2YS02M2JjMGNlM2FhNjFfMS5qcGc.jpg')";
+            document.querySelector('body').style.backgroundImage="url('https://static.vecteezy.com/system/resources/thumbnails/042/195/728/small/ai-generated-rainy-sky-background-free-photo.jpg')";
         }else if(data6==="Clear"){
             document.querySelector('body').style.backgroundImage="url('https://plus.unsplash.com/premium_photo-1733317236155-b0e1a2930f37?q=80&w=1170&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')";
         }else if(data6==="Thunderstorm"){
